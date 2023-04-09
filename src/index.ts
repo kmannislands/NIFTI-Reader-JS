cb62ec4ee118c153dcefac4f23542538d230b34f
Kieran Mann <kieran.mann@o8t.com>
2023-04-09 14:57:31 -0700

    adjust exports--tests passing

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,5 @@
-export { Utils } from './utilities';
-export { NiftiExtension } from './nifti-extension';
-
-
-// TODO: replace the following with an export statement once more files have been converted
-import { nifti } from './nifti';
-
-export const isCompressed = nifti.isCompressed;
-export const readHeader = nifti.readHeader;
-export const readImage = nifti.readImage;
-export const decompress = nifti.decompress;
-export const isNIFTI = nifti.isNIFTI;
-export const isNIFTI1 = nifti.isNIFTI1;
-export const isNIFTI2 = nifti.isNIFTI2;
-export const hasExtension = nifti.hasExtension;
-export const readExtensionData = nifti.readExtensionData;
+export { Utils } from "./utilities";
+export { NiftiExtension } from "./nifti-extension";
+export { NIFTI1 } from "./nifti1";
+export { NIFTI2 } from "./nifti2";
+export * from "./nifti";
