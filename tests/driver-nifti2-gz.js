38472f27d2f40bea6347eb0e1ee05556342af590
Kieran Mann <kieran.mann@o8t.com>
2023-04-09 13:20:37 -0700

    use VSCode auto refactor to convert tests to ESM

--- tests/driver-nifti2-gz.js
+++ tests/driver-nifti2-gz.js
@@ -4,13 +4,13 @@
 
 "use strict";
 
-var assert = require("assert");
-var fs = require('fs');
+import { equal, doesNotThrow, deepEqual } from "assert";
+import { readFileSync } from 'fs';
 
-var nifti = require('../src/nifti.js');
+import { Utils, isCompressed, decompress, isNIFTI1, isNIFTI2, readHeader, readImage } from '../src/nifti.js';
 
-var buf = fs.readFileSync('./tests/data/avg152T1_LR_nifti2.nii.gz');
-var data = nifti.Utils.toArrayBuffer(buf);
+var buf = readFileSync('./tests/data/avg152T1_LR_nifti2.nii.gz');
+var data = Utils.toArrayBuffer(buf);
 var nifti2 = null;
 var bytes = null;
 var clone = null;
@@ -18,53 +18,53 @@ var clone = null;
 describe('NIFTI-Reader-JS', function () {
     describe('compressed nifti-2 test', function () {
         it('isCompressed() should return true', function () {
-            assert.equal(true, nifti.isCompressed(data));
+            equal(true, isCompressed(data));
         });
 
         it('should not throw error when decompressing', function (done) {
-            assert.doesNotThrow(function() {
-                data = nifti.decompress(data);
+            doesNotThrow(function() {
+                data = decompress(data);
                 done();
             });
         });
 
         it('isNIFTI1() should return false', function () {
-            assert.equal(false, nifti.isNIFTI1(data));
+            equal(false, isNIFTI1(data));
         });
 
         it('isNIFTI2() should return true', function () {
-            assert.equal(true, nifti.isNIFTI2(data));
+            equal(true, isNIFTI2(data));
         });
 
         it('should not throw error when reading header', function (done) {
-            assert.doesNotThrow(function() {
-                nifti2 = nifti.readHeader(data);
+            doesNotThrow(function() {
+                nifti2 = readHeader(data);
                 done();
             });
         });
 
         it('dims[1] should be 91', function () {
-            assert.equal(91, nifti2.dims[1]);
+            equal(91, nifti2.dims[1]);
         });
 
         it('dims[2] should be 109', function () {
-            assert.equal(109, nifti2.dims[2]);
+            equal(109, nifti2.dims[2]);
         });
 
         it('dims[3] should be 91', function () {
-            assert.equal(91, nifti2.dims[3]);
+            equal(91, nifti2.dims[3]);
         });
 
         it('image data checksum should equal 471047545', function () {
-            var imageData = nifti.readImage(nifti2, data);
-            var checksum = nifti.Utils.crc32(new DataView(imageData));
-            assert.equal(checksum, 471047545);
+            var imageData = readImage(nifti2, data);
+            var checksum = Utils.crc32(new DataView(imageData));
+            equal(checksum, 471047545);
         });
 
         it('data returned from toArrayBuffer preserves all nifti-2 properties', function() {
             bytes = nifti2.toArrayBuffer();
-            clone = nifti.readHeader(bytes);
-            assert.deepEqual(clone, nifti2);
+            clone = readHeader(bytes);
+            deepEqual(clone, nifti2);
         });
 
     });
